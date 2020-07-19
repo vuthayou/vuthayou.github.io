8769b12353af8917bf985fdfac0c07bb46c7cda6
Vutha You <vuthayou16@gmail.com>
2020-07-19 02:06:35 -0600

    Base code for script generator done

---
+++ app.js
@@ -0,0 +1,37 @@
+var i = 0;
+var z = 0;
+const MAXNUM = 4;
+function autoWrite(s) {
+    $("#output").append(s);
+}
+
+$(document).ready(function() {
+    var script = "";
+    var j = 0;
+    var numChars = 0;
+    var tmpStr = "";
+    jQuery.get('script.txt', function(data) {
+        script = data;
+        
+        //call autowrite when key is pressed
+        $("#input").keypress(function() {
+            if (j < script.length) {
+                numChars = Math.floor(Math.random() * MAXNUM) + 1;
+                console.log("Random chars # is " + numChars);
+                for (var i = j; i < j + numChars; i++) {
+                    console.log(i);
+                    tmpStr += script[i];
+                }
+                j += numChars;
+                
+                console.log(tmpStr);
+                autoWrite(tmpStr);
+                
+            }
+            tmpStr = ""; //clearing string
+        })
+        
+    });
+    
+});
+
